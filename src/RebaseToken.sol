eb3b67b78e16735549448d79cb714c22803983c3
asingizwe1 <asingizwe1louis1@gmail.com>
2026-02-10 05:40:32 +0300

    getter for interest rate , and mint fucntion set up

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -11,16 +11,51 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  */
 contract RebaseToken is ERC20
 {
+    ////////////////
+    //Errors
     error RebaseToken__InterestRateCanOnlyDecrease();
+
+     ////////////////
+    //STATE VARIABLES
     uint256 private s_interestRate=5e10;//you can work with decimals in solidity 
     //50/100 *1e18 = 5e10
+mapping (address=>uint256) public s_userInterestRate;
+
+
+     ////////////////
+    //EVENTS
+    event InterestRateSet(uint256 newInterestRate);
+
 constructor() ERC20("RebaseToken","RCT"){}
+
+/**
+ * @notice Set the interest rate in the contract
+ * @param _newInterestRate The ne interest rate to be set
+ * @dev The interest rate can only decrease
+ * 
+ */
 function setInterestRate(uint256 _newInterestRate) external{
 if (_newInterestRate<s_interestRate){
-    emit RebaseToken__InterestRateCanOnlyDecrease();
+    revert RebaseToken__InterestRateCanOnlyDecrease();
 }
      s_interestRate = _newInterestRate;
-    
+    emit InterestRateSet(_newInterestRate);
+
+}
+
+/**
+ * @notice get interest rate for user
+ * 
+ * 
+ */
+function mint(address _to, uint256 _amount) external{
+    // at point of minting we want to set our own interest rate
+_mint(_to,_amount);//from openzeppelin
+
+}
+
+function getUserInterestRate(address _user) exterbal view returns (uint256){
+return s_userInterstRate[_user];
 
 }
 
