a5410c62ad4726a55854a1974939042fc34bcbcc
asingizwe1 <asingizwe1louis1@gmail.com>
2026-02-10 05:22:43 +0300

    rbt

---
+++ src/RebaseToken.sol
@@ -0,0 +1,9 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract RebaseToken is ERC20
+{
+constructor() ERC20("RebaseToken","RCT"){}
+
+
+}
