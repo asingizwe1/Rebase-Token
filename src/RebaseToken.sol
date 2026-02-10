3436f23c920ab5b819b5618b38ad1a6960dce93f
asingizwe1 <asingizwe1louis1@gmail.com>
2026-02-10 05:29:32 +0300

    setting up setInterestRate function and interest rate error

--- src/RebaseToken.sol
+++ src/RebaseToken.sol
@@ -1,9 +1,27 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @title RebaseToken
+ * @author Louis
+ * @notice This is a rebase token that incentivises users to deposit
+ * @notice Interest rate can only deposit
+ * @notice each user will have global interest rate at the point of deposit
+ */
 contract RebaseToken is ERC20
 {
+    error RebaseToken__InterestRateCanOnlyDecrease();
+    uint256 private s_interestRate=5e10;//you can work with decimals in solidity 
+    //50/100 *1e18 = 5e10
 constructor() ERC20("RebaseToken","RCT"){}
+function setInterestRate(uint256 _newInterestRate) external{
+if (_newInterestRate<s_interestRate){
+    emit RebaseToken__InterestRateCanOnlyDecrease();
+}
+     s_interestRate = _newInterestRate;
+    
 
+}
 
 }
