4ff25cddee6edec9425f1855cf938596642e3ddf
asingizwe1 <asingizwe1louis1@gmail.com>
2026-02-19 09:06:40 +0300

    final setup of functions events and errors in vault

--- src/Vault.sol
+++ src/Vault.sol
@@ -3,32 +3,59 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity 0.8.24;
 
+import {IRebaseToken} from "./interfaces/IRebaseToken.sol";
+
+//LOOK AT THE LOWLEVEL CALL AT REDEEM AND UNDERSTAND IT 
+
 contract Vault{
 //we need to pass the token address to the constructor
 //create a deposit and mint function
 //creat redeem, burn tokens from the user and sends the user ETH
 
-address private immutable _rebaseToken;
+IRebaseToken private immutable _rebaseToken;
 
+//indexed helps you sort the events
 event Deposit(address indexed user,uint256 amount);
+//user is indexed because we want to see number of times they have redeemed 
+event Redeem(address indexed user,uint256 amount);
 
-constructor(address _rebase)
+
+   error Vault__RedeemFailed();
+
+
+constructor(IRebaseToken _rebaseToken)
 {
 i_rebaseToken=_rebaseToken
 }
 //adding a fallback
 receive() external payable{}
 
+/**
+ *  @notice allows users deposit and mint rebase tokens
+ * 
+ */
 function deposit() external payable
 {
-    i_rebaseToken.mint(msg.sender,msg.value);
+  // IrebaseToken( i_rebaseToken) we can do this but its a better practice of putting it in constructor
+   i_rebaseToken.mint(msg.sender,msg.value);
     emit Deposit(msg.sender,msg.value);
 }
 
 
+function redeem() external
+{//1 burn
+i_rebaseToken.burn(msg.sender,i_rebaseToken.balanceOf(msg.sender));
+//2 send them eth
+(bool success,)=payable(msg.sender).call{value:_amount}("");
+//since no returnj data we leave it as bool,
+if(!success){
+    revert Vault__RedeemFailed();
+}
+emit Redeem(msg.sender,_amount);
+}
 
 function getRebaseTokenAddress() external view returns(address)
 {
-    return i_rebaseToken;
+    return address(i_rebaseToken);
 }
 }
