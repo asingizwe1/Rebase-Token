5097f227f8616e6def3b1981ade77c470d866993
asingizwe1 <asingizwe1louis1@gmail.com>
2026-02-19 08:51:31 +0300

    creating interface for rebasetoken holding functions to be modified

--- src/Vault.sol
+++ src/Vault.sol
@@ -9,12 +9,23 @@ contract Vault{
 //creat redeem, burn tokens from the user and sends the user ETH
 
 address private immutable _rebaseToken;
+
+event Deposit(address indexed user,uint256 amount);
+
 constructor(address _rebase)
 {
 i_rebaseToken=_rebaseToken
 }
 //adding a fallback
 receive() external payable{}
+
+function deposit() external payable
+{
+    i_rebaseToken.mint(msg.sender,msg.value);
+    emit Deposit(msg.sender,msg.value);
+}
+
+
 
 function getRebaseTokenAddress() external view returns(address)
 {
