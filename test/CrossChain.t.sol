eff03bb07a9239f6285eb3aa7dbe02ae40cc81be
asingizwe1 <asingizwe1louis1@gmail.com>
2026-03-21 22:29:08 +0300

    TEST

--- test/CrossChain.t.sol
+++ test/CrossChain.t.sol
@@ -10,11 +10,17 @@ import {IRebaseToken} from "..src/interfaces/IRebaseToken.sol";
 import {CCIPLocalSimulatorFork} from "@chainlink-local/src/ccip/CCIPLocalSimulatorFork.sol";
 
 contract CrossChainTest is Test{
+    address constant owner=makeAddr("owner");
 uint256 sepoliaFork;
 uint256 arbSepoliaFork;
 
 CCIPLocalSimulatorFork ccipLocalSimulatorFork;
 
+RebaseToken sepoliaToken;
+RebaseToken arbSepoliaToken;
+
+//deploying the vault
+Vault vault;
 function setUp() public
 {
 //we shall use our forked objects alot of the time
@@ -23,6 +29,20 @@ arbSepoliaFork=vm.createFork("arb-sepolia");//what you names your url in foundry
 
 ccipLocalSimulatorFork=new CCIPLocalSimulatorFork();
 vm.makePersistent(address(ccipLocalSimulator));
+
+//deploy and configure on sepolia
+vm.startPrank(owner);
+//we deploy new token to chain
+sepoliaToken=new RebaseToken();
+vault= new Vault(IRebaseToken(sepoliaToken));//users should be able to deposit to source chain
+vm.stopPrank();
+
+//deploy and configure on ArbSepolia
+vm.selectFork(arbSepoliaFork);
+arbSepoliaToken=new RebaseToken();
+vm.startPrank(owner);
+vm.stopPrank();
+
 }
 //create a way to simulate crosschain transactions
 //chainlink cross chain local helps to achieve this
