7c680963f60427c6a3da2f9793b22a9c73df427d
asingizwe1 <asingizwe1louis1@gmail.com>
2026-05-17 23:30:26 +0300

    final script set up

--- script/Deployer.s.sol
+++ script/Deployer.s.sol
@@ -1,7 +1,8 @@
 //SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.19;
-
+import {RegistryModuleOwnerCustom} from "@ccip/tokenAdminRegistry/RegistryModuleOwnerCustom.s.sol";
+import {TokenAdminRegistry} from "@ccip/tokenAdminRegistry/TokenAdminRegistry.sol";
 import {Script} "forge-std/Script.sol";
 import {IRebaseToken} from "../src/interfaces/IRebaseToken.sol";
 import {Deployer} from "../src/Deployer.sol";
@@ -11,7 +12,7 @@ import {CCIPLocalSimulatorFork,Register} from "@chainlink-local/src/CCIPLocalSim
 import {RebaseToken} from "../src/RebaseToken.s.sol";
 import {IERC20} from "@openzeppelin/contracts@4.8.3/token/ERC20/IERC20.sol";
 //steps in the ccip documentation /Enable your tokens in CCIP(burn&mint):Register from anEOA using foundry
-contract TokenAndPoolDeployer is Script{
+contract TokenAndPoolDeployer is Script{//script to deploy the token annd the pool
 function run() public returns (RebaseToken token, rebaseTokenPool pool) //returns both our contracts deployed
 {
  Register.NetworkDetails networkDetails = ccipLocalSimulatorFork.getNetworkDetails(block.chainid); ///chain id of script which the script is being run on 
@@ -19,8 +20,12 @@ function run() public returns (RebaseToken token, rebaseTokenPool pool) //return
 vm.startBroadcast(); // start sending transactions
 token=new RebaseToken();
 pool = new RebaseTokenPool(IERC20(address(token), new address[](0), networkDetails.rnmProxyAddress,networkDetails.routerAddress) );// the parameters we put are what is in the constructor in the contract
+token.grantMintAndBurnRole(address(pool)); //grant the pool contract the mint and burn role on the rebase token
 //IERC20(address(token) - //Treat token as something that follows the IERC20 interface.
 //an address alone doesnt have erc20 function
+RegistryModuleOwnerCustom(networkDetails.registryModuleOwnerCustom).registerAdminViaOwner(address(token));//who ever calls this will be the owner //we have registered ad,min via this
+TokenAdminRegistry(networkDetails.tokenAdminRegistry).acceptAdminRole(address(token)); //register the token in the registry
+TokenAdminRegistry(networkDetails.tokenAdminRegistry).setPool(address(token),address(pool)); //grant the pool contract the admin role on the token in the registry
 vm.stopBroadcast();
 }
 }
