1b49523e4990407018c1fdb65d5dc35905b804f7
asingizwe1 <asingizwe1louis1@gmail.com>
2026-05-20 23:40:00 +0300

    setting up the token pool

--- script/ConfigurePool.sol
+++ script/ConfigurePool.sol
@@ -1,2 +1,12 @@
  //script to configure the token pools
 //SPDX License-Identifier: MIT
+pragma solidity ^0.8.19;
+import {Script} "forge-std/Script.sol";
+
+contract ConfigurePool is Script {
+    function run(address _pool, address _vault) external {
+        vm.startBroadcast();
+        RebaseTokenPool(_pool).setVault(_vault); //set the vault address in the pool contract
+        vm.stopBroadcast();
+    }
+}
