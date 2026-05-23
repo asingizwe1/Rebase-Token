d0b5eb3f5e275bc9d4a601e266a13071a8948204
asingizwe1 <asingizwe1louis1@gmail.com>
2026-05-23 22:32:26 +0300

    setting up Router and router address

--- script/BridgeTokens.s.sol
+++ script/BridgeTokens.s.sol
@@ -1,8 +1,16 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
-
+import {IRouterClient} from "@ccip/contracts/src/v0.8/ccip/interfaces/IRouterClient.sol";
+//it contains functions router is calling
 contract BridgeTokens is Script{
+function run(){
+vm.startBroadcast();
+//create ccip message, approve the router to spend tokens approve router to spend our fees
+IRouterClient(routerAddress).ccipSend{value: fees}(destinationChainSelector, destinationAddress, message, feeToken, 0);
+//that address will depend on which chain we are running the script
 
-    
+vm.stopBroadcast();
+}
+
 }
